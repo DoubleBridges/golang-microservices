bfef399f1ae9f258a7d53d59c88dfe6f50857ea3
Dave Kane <davelkane@gmail.com>
2020-11-03 23:32:17 -0600

    initial commit after clone

--- introduction/webserver/main.go
+++ introduction/webserver/main.go
@@ -5,9 +5,11 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/hello", func(writer http.ResponseWriter, request *http.Request) {
-		writer.Write([]byte("hello world!"))
-	})
+	http.HandleFunc(
+		"/hello",
+		func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("hey buddy!"))
+	}	)
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
